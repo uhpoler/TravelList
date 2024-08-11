535d38d56b17a873f14ba9bcb660dd0aea614a3e
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-11 21:28:20 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+export default function App() {}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do </h3>
+    </div>
+  );
+}
+
+function PackingList() {}
+
+function Stats() {}
